f203e7af85fe5abc6464ec7e331c9071fd6b52a4
Andrey <zhegalovan@mail.ru>
2020-09-22 09:52:48 -0400

    added multiply main jar file in the gradle

--- Backend/src/main/java/cityguide/backend/ShowPlaceDataCollector.java
+++ Backend/src/main/java/cityguide/backend/ShowPlaceDataCollector.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import cityguide.backend.config.BackendConfig;
 import cityguide.backend.service.ShowPlaceDataCollectorService;
 
-public class DataStorageExecutor {
+public class ShowPlaceDataCollector {
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(BackendConfig.class, ShowPlaceDataCollectorService.class);
         final var dataCollectorService = ctx.getBean(ShowPlaceDataCollectorService.class);
