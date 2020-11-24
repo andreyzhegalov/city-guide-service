faf81e45e5453ceb5c3f8676f98b1c73c0011d19
Andrey <zhegalovan@mail.ru>
2020-11-24 01:58:48 -0500

    changed result message format

--- DataStorage/src/main/java/cityguide/datastorage/view/TelegramMessageViewImpl.java
+++ DataStorage/src/main/java/cityguide/datastorage/view/TelegramMessageViewImpl.java
@@ -1,11 +1,11 @@
 package cityguide.datastorage.view;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
-import cityguide.datastorage.model.Description;
 import cityguide.datastorage.model.ShowPlace;
 
 @Service
@@ -13,9 +13,17 @@ public class TelegramMessageViewImpl implements ShowPlaceView {
 
     @Override
     public String prepareMessage(List<ShowPlace> showPlaces) {
-        // NOTE only first description at this time
-        return (showPlaces.isEmpty()) ? "Ничего не найдено"
-                : showPlaces.get(0).getDescriptionList().stream().map(Description::getInfo)
-                        .collect(Collectors.joining(System.lineSeparator())).toString();
+        if (showPlaces.isEmpty()) {
+            return "Ничего не найдено";
+        }
+        final List<String> resultList = new ArrayList<String>();
+        for (final var showPlace : showPlaces) {
+            final var address = showPlace.getAddressString();
+            final var descriptions = showPlace.getDescriptionList().stream().map(description -> {
+                return address + System.lineSeparator() + description.getInfo();
+            }).collect(Collectors.toList());
+            resultList.addAll(descriptions);
+        }
+        return resultList.stream().collect(Collectors.joining(System.lineSeparator()));
     }
 }
--- DataStorage/src/test/java/cityguide/datastorage/view/TelegramMessageViewImplTest.java
+++ DataStorage/src/test/java/cityguide/datastorage/view/TelegramMessageViewImplTest.java
@@ -3,7 +3,9 @@ package cityguide.datastorage.view;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,22 +16,32 @@ class TelegramMessageViewImplTest {
 
     @Test
     void prepareMessageForNotFoundShowPlace() {
-        assertThat(new TelegramMessageViewImpl().prepareMessage(Collections.emptyList())).isEqualTo("Ничего не найдено");
+        assertThat(new TelegramMessageViewImpl().prepareMessage(Collections.emptyList()))
+                .isEqualTo("Ничего не найдено");
     }
 
-    private ShowPlace prepareShowPlace(){
+    private ShowPlace prepareShowPlace() {
         final var showPlace = new ShowPlace();
         final var descriptionList = new ArrayList<Description>();
+
         final var description = new Description();
         description.setInfo("some description");
         descriptionList.add(description);
+
+        final var description2 = new Description();
+        description2.setInfo("some description2");
+        descriptionList.add(description2);
+
         showPlace.setDescriptionList(descriptionList);
+        showPlace.setAddressString("house1");
         return showPlace;
     }
 
     @Test
-    void prepareMessage(){
+    void messageShouldContainAddress() {
         final var showPlaceList = Collections.singletonList(prepareShowPlace());
-        assertThat(new TelegramMessageViewImpl().prepareMessage(showPlaceList)).isEqualTo("some description");
+        final var messageLines = Arrays.asList("house1", "some description", "house1", "some description2");
+        final var resultMessage = messageLines.stream().collect(Collectors.joining(System.lineSeparator()));
+        assertThat(new TelegramMessageViewImpl().prepareMessage(showPlaceList)).isEqualTo(resultMessage);
     }
 }
